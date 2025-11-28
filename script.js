b1317e05893db4a88dbad511d90b471b4ec96b68
Ruben Sanchez <rubensanchez.g@outlook.com>
2025-11-27 22:14:01 -0600

    Version piloto del portafolio virtual de Ruben Sanchez

---
+++ script.js
@@ -0,0 +1,46 @@
+// Mostrar un mensaje de bienvenida en consola
+console.log("Bienvenido al portafolio de Rubén Sánchez 😎");
+
+// prueba de Efecto visual: cambia el color del header al hacer scroll
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    header.style.backgroundColor = window.scrollY > 50 ? "#192a56" : "#273c75";
+});
+// seccion dinamica de proyectos donde se crea una arreglo de proyectos y se muestran en el html
+
+// 1. Definimos un arreglo de proyectos
+const proyectos = [
+{
+    titulo: "Sistema de Gestión de Inventario (SGI)",
+    descripcion: "Aplicación para controlar materiales, entradas y salidas en almacén, con reportes automáticos.",
+    tecnologias: ["MySQL", "C#", "Windows Forms."],
+    link: "/html/sgi.html"
+},
+{
+//esapcio para agregar otro proyecto
+},
+{
+//espacio para agregar otro proyecto
+}
+];
+
+// 2. Obtenemos el contenedor donde se mostrarán los proyectos
+const contenedor = document.getElementById("lista-proyectos");
+
+// 3. Recorremos el arreglo y creamos las tarjetas
+proyectos.forEach(proyecto => {
+  // Creamos un elemento div para cada tarjeta
+const card = document.createElement("div");
+card.classList.add("proyecto");
+
+  // Generamos el contenido HTML de la tarjeta
+card.innerHTML = `
+    <h3>${proyecto.titulo}</h3>
+    <p>${proyecto.descripcion}</p>
+    <p><strong>Tecnologías:</strong> ${proyecto.tecnologias.join(", ")}</p>
+    <a href="${proyecto.link}" >Ver proyecto</a>
+`;
+
+  // Agregamos la tarjeta al contenedor
+contenedor.appendChild(card);
+});
